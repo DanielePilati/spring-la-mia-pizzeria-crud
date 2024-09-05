e65d2702a0632485b74371d33740066a27de44c0
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-09-05 16:48:09 +0200

    fix

--- src/main/java/org/web/app/java/spring/controller/FoodController.java
+++ src/main/java/org/web/app/java/spring/controller/FoodController.java
@@ -21,58 +21,55 @@ import jakarta.validation.Valid;
 @Controller
 @RequestMapping("/foods")
 public class FoodController {
-	
+
 	@Autowired
 	private FoodRepository repo;
-	
-	
+
 	@GetMapping()
 	public String index(Model model) {
-		
+
 		List<Food> foods = repo.findAll();
 		model.addAttribute("foods", foods);
 		model.addAttribute("search", new Search());
-	
+
 		return "/foods/index";
 	}
-	
+
 	@GetMapping("/show/{id}")
 	public String showFood(Model model, @PathVariable("id") Integer foodId) {
-		
+
 		model.addAttribute("foods", repo.findById(foodId).get());
-		
+
 		return "/foods/show";
 	}
+
 	@GetMapping("/search/")
 	public String searchName(Model model, @RequestParam("name") String name) {
-		
+
 		model.addAttribute("search", new Search());
 		model.addAttribute("foods", repo.findByNameContains(name));
-		
+
 		return "/foods/index";
 	}
-	
+
 	@GetMapping("/create")
 	public String create(Model model) {
-		
-		model.addAttribute("formModel", new Food());
-		
+
+		model.addAttribute("food", new Food());
+
 		return "/foods/create";
 	}
-	
 
 	@PostMapping("/create")
-	public String add(@Valid @ModelAttribute("Food") Food formFood, Model model, BindingResult br ) {
+	public String add(@Valid @ModelAttribute("food") Food formFood, BindingResult br, Model model) {
 
-		if(br.hasErrors()) {
+		if (br.hasErrors()) {
 			return "/foods/create";
 		}
-		
+
 		repo.save(formFood);
-		
+
 		return "redirect:/foods";
 	}
-	
-	
 
 }
--- src/main/java/org/web/app/java/spring/model/Food.java
+++ src/main/java/org/web/app/java/spring/model/Food.java
@@ -1,5 +1,9 @@
 package org.web.app.java.spring.model;
 
+import java.time.Instant;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,7 +13,6 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
-
 @Entity
 @Table(name = "foods")
 public class Food {
@@ -17,21 +20,32 @@ public class Food {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@NotEmpty
-	@NotNull
 	@Column(name = "food_name", nullable = false)
 	private String name;
-	
+
+	@NotEmpty
 	@Column(name = "description", nullable = true, columnDefinition = "LONGTEXT")
 	private String description;
-	
+
 	@Column(name = "img_url", columnDefinition = "LONGTEXT")
 	private String imgUrl;
-	
+
 	@NotNull
 	@Column(name = "food_price", nullable = false)
 	private Double price;
+
+	@UpdateTimestamp
+	private Instant updatedAt;
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Instant updatedAt) {
+		this.updatedAt = updatedAt;
+	}
 
 	public Integer getId() {
 		return id;
